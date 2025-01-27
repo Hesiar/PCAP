95769c22ac98e05dd40eb5fd5cbd7a80dd1a860f
Armando Herrero <hesiar94@gmail.com>
2025-01-27 18:03:54 +0100

    Ejercicios del 27 de enero y del dia anterior subidos

---
+++ POO/herencia_simple.py
@@ -0,0 +1,19 @@
+
+class Saiyan:
+    
+    planeta = "Sadala"
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Goku(Saiyan):
+    pass
+
+class Vegeta(Saiyan):
+    pass
+
+
+        
+        
+
+        
